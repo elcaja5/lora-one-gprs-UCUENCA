620526cea284d14faef023fcd78f16e0a3af0657
Erick Cajamarca <erickcaja08@gmail.com>
2024-05-27 22:21:56 -0500

    First commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,20 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'LoRa one-gprs',
+  tagline: 'Vinculación-Servicio',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ucuenca.lora.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/LoRa-one-gprs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Universidad de Cuenca', // Usually your GitHub org/user name.
+  projectName: 'LoRa one-gprs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
